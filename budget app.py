c0cbff1a36d2d12bba420e0be55336035a73f3f7
Mavin <mavinpeteromondi@gmail.com>
2025-01-04 09:43:38 +0300

    version 5

--- budget app.py
+++ budget app.py
@@ -14,8 +14,7 @@ class Category:
             self.total_amount+=dep['amount']
 
     def withdraw(self, amount, description=''):
-        self.calculate_amounts()
-        if self.total_amount > amount:
+        if self.check_funds(amount):
             self.ledger.append({'amount': -amount, 'description': description})
             return True
         else:
@@ -26,17 +25,39 @@ class Category:
         self.calculate_amounts()
         return self.total_amount
 
+    def transfer(self, amount, Category):
 
+        if self.check_funds(amount):
+            self.ledger.append({'amount': -amount, 'description': f'Transfer to {Category.category}'})
+            Category.deposit(amount, f'transfer from {self.category}')
+            return True
+        else:
+            return False
+
+    def check_funds(self, amount):
+        self.calculate_amounts()
+        if self.total_amount < amount:
+            return False
+        else:
+            return True
+
+    def __str__(self):
+        title=f'{self.category:*^30}' + '\n'
+        spending="\n".join([(dep['description'][:23] + f'{(round(float(dep['amount']), 2)):.2f}'.rjust(30-len(dep['description'][:23]))) for dep in self.ledger])
+        self.calculate_amounts()
+        total='\n' + 'Total: '+ str(self.total_amount)
+        return title + spending + total
 
 
 
 def create_spend_chart(categories):
     pass
 
-food=Category('food')
-food.deposit(9000,'groceries')
-food.deposit(2000, 'alcohol')
-food.withdraw(10000, 'more groceries')
-food.deposit(4000, 'more alcohol')
 
-print(food.get_balance())
+food = Category('Food')
+food.deposit(1000, 'initial deposit')
+food.withdraw(10.15, 'groceries')
+food.withdraw(15.89, 'restaurant and more food for dessert')
+clothing = Category('Clothing')
+food.transfer(50, clothing)
+print(food)
